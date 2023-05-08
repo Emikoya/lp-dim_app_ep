31bfae171d36908f47bda6fee7dda2888f2eb35e
Emikoya <pruvost.lily@gmail.com>
2023-05-08 22:21:50 +0200

    app + firebase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'lp-dim-app-ep',
+    appId: '1:621922647071:web:c959e4a24ad14323aaa92a',
+    databaseURL: 'https://lp-dim-app-ep-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'lp-dim-app-ep.appspot.com',
+    apiKey: 'AIzaSyB-qrXgafIJxSfmXIjG3oantSTrzQBI8ek',
+    authDomain: 'lp-dim-app-ep.firebaseapp.com',
+    messagingSenderId: '621922647071',
+  },
     production: false,
    iutApiBaseUrl: 'https://lp-dim-api-ep.vercel.app'
 
